c47d9a8a2542da38791ae0d2a5fe5c74aed7e94f
David Tench <dtench@lbl.gov>
2026-01-14 12:54:52 -0800

    cleaned up hash fn change

--- src/jl_sketch.rs
+++ src/jl_sketch.rs
@@ -7,13 +7,12 @@ use std::ops::Mul;
 use crate::fasthash::FastHasher;
 use crate::{ffi, utils};
 
-use fasthash::xx::Hasher64 as XXHasher;
+use fasthash::murmur2::Hasher64_x64 as Murmur2Hasher;
 use ndarray::{Array1,Array2,s};
 use sprs::{CsMatI, CsMatViewI};
 use num_traits::cast;
 use utils::{CustomIndex, CustomValue};
 
-use fasthash::murmur2::Hasher64_x64 as Murmur2Hasher;
 
 //maps hash function output to {-1,1} evenly
 fn transform(input: i64) -> i64 {
@@ -30,17 +29,6 @@ pub fn add_to_position<IndexType: CustomIndex, ValueType: CustomValue>(matrix: &
         None => matrix.insert(row, col, val),
     }
 }
-
-//this function is used to find the value of a hash function seeded with seed, when given two positional arguments. Used to repeatably compute values in the JL sketch matrix
-// by passing the coordinates of the position as the two arguments.
-// pub fn hash_with_inputs(seed: u64, input1: u64, input2: u64) -> i64 {
-//    let mut checkhash = XXHasher::with_seed(seed);
-//     input1.hash(&mut checkhash);
-//     input2.hash(&mut checkhash);
-//     let result = checkhash.finish() as u32;
-//     //println!("{}",result);
-//     transform(result as i64)
-// }
 
 pub fn hash_with_inputs(seed: u64, input1: u64, input2: u64) -> i64 {
    let mut checkhash = Murmur2Hasher::with_seed(seed);
