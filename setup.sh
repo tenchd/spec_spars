e03d2f9b9ea988190d62c22a16bcf5e0bb5c18af
David Tench <dtench@dtench-l78.dhcp.lbl.gov>
2026-04-01 20:17:31 -0700

    setup script mk1

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+printf "fast_mtx_path = \"path/to/fast_matrix_market/include\" \ninput_files_path = \"path/to/input/files\" \noutput_files_path = \"path/to/output/files\"" > config.toml
