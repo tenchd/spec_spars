a7aefa7c2b20542b347a486a30973f8d92916bad
David Tench <dtench@dtench-l78.dhcp.lbl.gov>
2026-04-01 20:30:57 -0700

    setup downloads virus dataset

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# make a config.toml file with placeholder values, which the user will replace with the correct paths.
 printf "fast_mtx_path = \"path/to/fast_matrix_market/include\" \ninput_files_path = \"path/to/input/files\" \noutput_files_path = \"path/to/output/files\"" > config.toml
+
+# download the virus dataset and put it in the data directory.
+mkdir -p data
+cd data
+wget https://portal.nersc.gov/project/m1982/HipMCL/viruses/vir_vs_vir_30_50length_propermm.mtx
+mv vir_vs_vir_30_50length_propermm.mtx virus.mtx
